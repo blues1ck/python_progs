e8884dff60332c17e0fa2c05302f244409a31df4
Илья Усов <usov.is@phystech.edu>
2023-05-05 16:07:05 +0300

    доделанный chat(gpt)

--- contest_games_generators/5.py
+++ contest_games_generators/5.py
@@ -20,3 +20,53 @@
 если при исходном расположении фишки в i-й вершине первой доли, выигрывает Наташа
 и 'P', если выигрывает Петя. Вторая строка должна описывать вершины второй доли аналогичным образом.
 '''
+
+graph = {}
+with open('INPUT.TXT', 'r') as f:
+    n, m = map(int, f.readline().split())
+    graph = {}
+    for i in range(1, n+1):
+        graph[i] = []
+    for j in range(m):
+        u, v = map(int, f.readline().split())
+        graph[u].append(v)
+        graph[v].append(u)
+
+
+# Функция минимакса
+def minimax(node, depth, is_max):
+    if not graph[node]:
+        return 1 if is_max else -1
+    if depth == 10:
+        return 1 if is_max else -1
+    if is_max:
+        max_val = float('-inf')
+        for adj_node in graph[node]:
+            val = minimax(adj_node, depth+1, False)
+            max_val = max(max_val, val)
+        return max_val
+    else:
+        min_val = float('inf')
+        for adj_node in graph[node]:
+            val = minimax(adj_node, depth+1, True)
+            min_val = min(min_val, val)
+        return min_val
+
+# Определение победителя в каждой вершине первой доли
+winners = {}
+for node in graph.keys():
+    winners[node] = minimax(node, 0, True) > 0
+
+# Определение победителя в каждой вершине второй доли
+for node in graph.keys():
+    if not winners[node]:
+        continue
+    for adj_node in graph[node]:
+        if winners[adj_node]:
+            winners[node] = False
+            break
+
+# Вывод результатов
+with open('output.txt', 'w') as f:
+    for node in graph.keys():
+        f.write('1\n' if winners[node] else '0\n')
---
+++ contest_games_generators/6.py
@@ -0,0 +1,26 @@
+'''
+Реализуйте итератор колоды карт (52 штуки) CardDeck. Каждая карта представлена в виде строки типа «2 Пик».
+При вызове функции next() будет представлена следующая карта. По окончании перебора всех элементов возникнет ошибка StopIteration.
+
+'''
+class CardDeck:
+    def __init__(self):
+        self.suits = ['Пик', 'Треф', 'Бубен', 'Червей']
+        self.ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Валет', 'Дама', 'Король', 'Туз']
+        self.index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.suits) * len(self.ranks):
+            raise StopIteration
+        suit = self.suits[self.index // len(self.ranks)]
+        rank = self.ranks[self.index % len(self.ranks)]
+        self.index += 1
+        return f'{rank} {suit}'
+    
+deck = CardDeck()
+for card in deck:
+    print(card)
+print(next(deck))
