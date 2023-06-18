a75b1d008720ef87a33fa6aa3be3a839a6fb1b0b
Илья Усов <usov.is@phystech.edu>
2023-06-18 19:38:36 +0300

    with vpv

--- vpv_leto2023/graphs.py
+++ vpv_leto2023/graphs.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 file = '232.xlsx'
 
 s_kollectora = 23.6 * 10**(-4)
@@ -9,21 +12,28 @@ timestr = (df['t.1'].tolist())[:-2]
 time = [float(i) for i in timestr]
 temperaturestr = (df['T1'].tolist())[:-2]
 temperature = [float(i) for i in temperaturestr]
+temperature_K = [i+273 for i in temperature]
 tokstr = (df['I, mkA'].tolist())[:-2]
 tok_mkA = [float(i) for i in tokstr]
 rev_tempstr = (df['100/T'].tolist())[:-2]
-rev_temp = [float(i) for i in rev_tempstr]
+rev_temp = [float(10*i) for i in rev_tempstr]
 lnI2str = (df['lnI^2'].tolist())[:-2]
 lnI2 = [float(i) for i in lnI2str]
 tok_emissii = 0.5
 po_toka = [i/1000/s_kollectora for i in tok_mkA]
-
-import matplotlib.pyplot as plt
-import numpy as np
-
-def graph(x, y, x_name, y_name, function):
+lnI = [np.log(i) for i in tok_mkA] 
+n_str = (df['n'].tolist())[:len(rev_temp)]
+n = [float(i) for i in n_str]
+lnn = [np.log(float(i)) for i in n_str]
+theta_str = (df['theta'].tolist())[:len(rev_temp)]
+theta = [float(i) for i in theta_str]
+one_minus_theta = [1-i for i in theta]
+theta_delit_one_minus_theta = [i/j for i in theta for j in one_minus_theta]
+s_kollectora_x_theta = [i*s_kollectora for i in theta]
+def graph(x, y, x_name, y_name, function='none'):
     time = x
     tok_mkA = y
+    aprox = 0
     if function == 'exp':
         model = np.poly1d(np.polyfit(time, np.log(tok_mkA), deg = 1))
         aprox = f'{str(np.exp(model[0]))[:8]}*exp({str(model[1])[:8]}x)'
@@ -39,7 +49,10 @@ def graph(x, y, x_name, y_name, function):
         u = [k*t + b for t in v]
         plt.plot(v, u, '--', color = 'orange', label = 'linear aproximation')
 
-    plt.plot(time, tok_mkA, label = y_name + '(' + x_name +')' + f'\n aproximation: \n {aprox}', color = 'green')
+    if aprox:
+        plt.plot(time, tok_mkA, label = y_name + '(' + x_name +')' + f'\n aproximation: \n {aprox}', color = 'green')
+    else:
+        plt.plot(time, tok_mkA, label = y_name + '(' + x_name +')', color = 'green')
 
     plt.grid()
     plt.title(y_name + '(' + x_name +')')
@@ -51,8 +64,15 @@ def graph(x, y, x_name, y_name, function):
     plt.show()
     '''plt.savefig(f'{y_name}({x_name}).png')'''
 
-graph(time, tok_mkA, 'время,с', 'ток,мкА', 'exp')
+print(len(n))
+graph(theta_delit_one_minus_theta[:len(rev_temp)], n[:len(rev_temp)], 'theta/(1-theta)', 'n', 'linear')
+# graph(temperature_K, s_kollectora_x_theta, 'T, K', 's*theta')
+# graph(rev_temp, s_kollectora_x_theta, '1000/T', 's*theta')
+'''graph(time, tok_mkA, 'время,с', 'ток,мкА', 'exp')
 graph(temperature, tok_mkA, 'температура,C', 'ток,мкА', 'exp')
-graph(rev_tempstr[:-2], lnI2[:-2], '100/T', 'ln(I^2)', 'linear')
+graph(rev_temp[:-2], lnI2[:-2], '1000/T', 'ln(I^2)', 'none')
 graph(rev_temp[260:450], lnI2[260:450], '100/T', 'лучший участок ln(I^2)', 'linear')
-graph(temperature, po_toka, 'температура', 'плотность тока', 'exp')
+graph(rev_temp, lnI, 'ln(I)', '1000/T', 'none')
+graph(rev_temp, lnn, '1000/T', 'ln(n)', 'none')
+graph(rev_temp[260:450], lnn[260:450], '1000/T', 'ln(n)', 'linear')'''
+# graph(rev_temp[260:], lnI[260:], 'ln(I) при температурах активации', '1000/T', 'linear')
